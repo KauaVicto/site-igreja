4f0806d58c56d884ce7db5218f3a0b1e7fc76547
KauaVicto <kauavictor259@gmail.com>
2021-10-15 10:10:15 -0300

    add composer

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 session_start();
 
 $uri_parse = $_SERVER['REQUEST_URI'];
+require_once 'vendor/autoload.php';
 
-require_once 'controller/Devocional.php';
-require_once 'controller/Usuario.php';
-require_once 'controller/ConectBD.php';
 require_once 'rotas.php';
-
-?>
-
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'decb2cf9e99043ad6e396488b0fb856fd1501b1f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'decb2cf9e99043ad6e396488b0fb856fd1501b1f',
+            'dev_requirement' => false,
+        ),
+    ),
+);

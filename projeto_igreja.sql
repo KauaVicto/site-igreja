bfa7ea93a654247772ad3534320d1a1c7616b7aa
KauaVicto <kauavictor259@gmail.com>
2021-10-15 16:44:32 -0300

    update sql

--- projeto_igreja.sql
+++ projeto_igreja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Out-2021 às 16:24
+-- Tempo de geração: 15-Out-2021 às 20:41
 -- Versão do servidor: 10.4.21-MariaDB
 -- versão do PHP: 8.0.11
 
@@ -75,13 +75,34 @@ INSERT INTO `devocionais` (`id`, `assunto`, `descricao`, `data`, `arquivo`) VALU
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `possui_cargo`
+--
+
+CREATE TABLE `possui_cargo` (
+  `id_relacao` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_cargo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `possui_cargo`
+--
+
+INSERT INTO `possui_cargo` (`id_relacao`, `id_user`, `id_cargo`) VALUES
+(3, 1, 1),
+(4, 4, 0),
+(5, 3, 2),
+(6, 1, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `nome` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `id_cargo` int(11) DEFAULT NULL,
   `login` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `senha` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -90,10 +111,10 @@ CREATE TABLE `users` (
 -- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id_user`, `nome`, `id_cargo`, `login`, `senha`) VALUES
-(1, 'George', 1, 'george', '827ccb0eea8a706c4c34a16891f84e7b'),
-(3, 'Layla Adryellen Oliveira Gomes', 2, 'layla', '827ccb0eea8a706c4c34a16891f84e7b'),
-(4, 'Admin', 0, 'admin', '827ccb0eea8a706c4c34a16891f84e7b');
+INSERT INTO `users` (`id_user`, `nome`, `login`, `senha`) VALUES
+(1, 'George', 'george', '827ccb0eea8a706c4c34a16891f84e7b'),
+(3, 'Layla Adryellen Oliveira Gomes', 'layla', '827ccb0eea8a706c4c34a16891f84e7b'),
+(4, 'Admin', 'admin', '827ccb0eea8a706c4c34a16891f84e7b');
 
 --
 -- Índices para tabelas despejadas
@@ -112,11 +133,18 @@ ALTER TABLE `devocionais`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `possui_cargo`
+--
+ALTER TABLE `possui_cargo`
+  ADD PRIMARY KEY (`id_relacao`),
+  ADD KEY `fk_id_user` (`id_user`),
+  ADD KEY `fk_id_cargo` (`id_cargo`);
+
+--
 -- Índices para tabela `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`),
-  ADD KEY `fk_CargoUser` (`id_cargo`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -135,6 +163,12 @@ ALTER TABLE `devocionais`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
+-- AUTO_INCREMENT de tabela `possui_cargo`
+--
+ALTER TABLE `possui_cargo`
+  MODIFY `id_relacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
@@ -145,10 +179,11 @@ ALTER TABLE `users`
 --
 
 --
--- Limitadores para a tabela `users`
+-- Limitadores para a tabela `possui_cargo`
 --
-ALTER TABLE `users`
-  ADD CONSTRAINT `fk_CargoUser` FOREIGN KEY (`id_cargo`) REFERENCES `cargos` (`id_cargo`);
+ALTER TABLE `possui_cargo`
+  ADD CONSTRAINT `fk_id_cargo` FOREIGN KEY (`id_cargo`) REFERENCES `cargos` (`id_cargo`),
+  ADD CONSTRAINT `fk_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
